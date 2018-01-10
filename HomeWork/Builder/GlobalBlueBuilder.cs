b216c0121e4c4856a13ddcdacd92bad3c45d2989
Alexander <dreadnight@tut.by>
2018-01-10 23:06:14 +0300

    Add Builder

---
+++ HomeWork/Builder/GlobalBlueBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class GlobalBlueBuilder : IBuilder
+    {
+        private Check check = new Check();
+
+        public Check GetCheck()
+        {
+            return check;
+        }
+
+        public IBuilder SetPercentTax(decimal percent)
+        {
+            check.PercentTax = percent;
+            return this;
+        }
+
+        public IBuilder SetDate(DateTime date)
+        {
+            check.Date = date;
+            return this;
+        }
+
+        public IBuilder SetSum(decimal sum)
+        {
+            check.Sum = sum;
+            return this;
+        }
+    }
+}
