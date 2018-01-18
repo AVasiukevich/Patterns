d47fd5f2c85f3b7f948530213a9f6dfc61cd55c6
Alexander <dreadnight@tut.by>
2018-01-19 00:09:43 +0300

    Add pattern

---
+++ HomeWork/Composite/Document.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Composite
+{
+    class Document : IXmlElements
+    {
+        private List<IXmlElements> _list = new List<IXmlElements>();
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public void AddElement(IXmlElements element)
+        {
+            _list.Add(element);
+        }
+
+        public XElement CreateElement()
+        {
+            var elements = new XElement(Name, Value);
+            foreach (var item in _list)
+            {
+                elements.Add(item.CreateElement());
+            }
+            return elements;
+        }
+
+        public void ShowElement()
+        {
+            Console.WriteLine($"<{Name}> {Value}");
+            foreach (var item in _list)
+            {
+                item.ShowElement();
+            }
+            Console.WriteLine($"</{Name}>");
+        }
+        public void SaveXML(string path)
+        {
+            CreateElement().Save(path);
+        }
+    }
+}
--- HomeWork/Composite/Program.cs
+++ HomeWork/Composite/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Composite
 {
@@ -10,6 +11,17 @@ namespace Composite
     {
         static void Main(string[] args)
         {
+            var document = new Document() { Name="First"};
+            var element = new SingleElement() { Name = "Sub_1", Value = "Sub" };
+            var document_2 = new Document() { Name = "Second" };
+            var element_2 = new SingleElement() { Name = "Sub_2", Value = "Sub" };
+            document_2.AddElement(element_2);
+            document.AddElement(element);
+            document.AddElement(document_2);
+            document.ShowElement();
+
+            document.SaveXML("text.xml");
+            Console.ReadLine();
         }
     }
 }
