9b31c0737c187c6c938d1ade2f7e83928aab4402
Alexander <dreadnight@tut.by>
2018-01-19 21:05:58 +0300

    Add ready project

--- HomeWork/Flyweight/MainWindow.xaml.cs
+++ HomeWork/Flyweight/MainWindow.xaml.cs
@@ -20,9 +20,25 @@ namespace Flyweight
     /// </summary>
     public partial class MainWindow : Window
     {
+        CharacterFactory factory = new CharacterFactory();
         public MainWindow()
         {
             InitializeComponent();
+            factory.GetCharacter('A', new BitmapImage(new Uri("alpha.png", UriKind.Relative)));
+            factory.GetCharacter('B', new BitmapImage(new Uri("beta.png", UriKind.Relative)));
+            factory.GetCharacter('H', new BitmapImage(new Uri("eta.png", UriKind.Relative)));
+            factory.GetCharacter('K', new BitmapImage(new Uri("kappa.png", UriKind.Relative)));
+            factory.GetCharacter('O', new BitmapImage(new Uri("omicron.png", UriKind.Relative)));
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            uniform.Children.Clear();
+            uniform.Columns = tb_main.Text.Length;
+            var _chars = tb_main.Text.ToCharArray();
+            for (int i = 0; i < tb_main.Text.Length; i++)
+                    uniform.Children.Add(new Image() { Source = ((LatinCharacter)factory.GetCharacter(_chars[i])).picture});
+            tbl.Text = String.Format($"Размер словаря: {factory.Count()}. ABHKO внесены с image!");           
         }
     }
 }
