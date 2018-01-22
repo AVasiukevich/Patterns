06aed756b1c7237d96b3111f025c08e4ddee7bbf
Alexander <dreadnight@tut.by>
2018-01-22 23:29:03 +0300

    Add ready project

---
+++ HomeWork/Façade/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Façade
+{
+    class Factorial
+    {
+        Func<long, BigInteger> _factorial = null;
+
+        public BigInteger GetFactorial(int number)
+        {
+            _factorial = x => x == 0 ? 1 : x * _factorial(x - 1);
+            return _factorial(number);
+        }
+
+    }
+}
