9358ca9071daaedb4e35d7eaf3e94575993f458c
Alexander <dreadnight@tut.by>
2018-01-24 13:03:57 +0300

    Add ready project

---
+++ HomeWork/Observer/A.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class A : IObservable<A>
+    {
+        private int _value;
+        private readonly IList<IObserver<A>> _listeners = new List<IObserver<A>>();
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    Notify();
+                }
+            }
+        }
+        public IDisposable Subscribe(IObserver<A> observer)
+        {
+            _listeners.Add(observer);
+            return new Unsubscriber(this, observer);
+        }
+
+        private void Notify()
+        {
+            foreach (var listener in _listeners)
+            {
+                listener.OnNext(this);
+            }
+        }
+
+        private class Unsubscriber : IDisposable
+        {
+            private readonly A _sensor;
+            private readonly IObserver<A> _listener;
+
+            public Unsubscriber(A sensor, IObserver<A> listener)
+            {
+                _sensor = sensor;
+                _listener = listener;
+            }
+
+            public void Dispose()
+            {
+                _sensor._listeners.Remove(_listener);
+            }
+        }
+    }
+}
---
+++ HomeWork/Observer/B.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class B : IObserver<A>
+    {
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(A a)
+        {
+            using (FileStream fs = new FileStream("text.txt", FileMode.Append, FileAccess.Write))
+            {
+                byte[] writeBytes = Encoding.UTF8.GetBytes(String.Format($"{a.Value} - {DateTime.Now}") + Environment.NewLine);
+                fs.Write(writeBytes, 0, writeBytes.Length);
+            }
+        }
+    }
+}
