51f3339f31462520c58300aac345e4ba42431dad
Alexander <dreadnight@tut.by>
2018-01-24 11:37:58 +0300

    Add ready project

---
+++ HomeWork/TemplateMethod/Sсhool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class Sсhool : EducationTM
+    {
+        public Sсhool(int examEnter, int examAttestat):base(examEnter,examAttestat)
+        {}
+
+        protected override bool Enter(int exam)
+        {
+            if (exam > 0 && exam <= 10)
+                return true;
+
+            return false;
+        }
+
+        protected override bool PassExams(int exam)
+        {
+            if (exam > 4 && exam <= 10)
+                return true;
+
+            return false;
+        }
+    }
+}
