750f313938984161fb7958a0469b606945646921
Alexander <dreadnight@tut.by>
2018-01-31 16:52:04 +0300

    Add ready project

---
+++ Exam/Task_2/HtmlElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class HtmlElement : IHtmlElement
+    {
+        public string Info { get; set; }
+        public string Name { get; set; }
+
+        public string Print()
+        {
+            return String.Format($"<html>\n<head>\n\t<title>{Name}</title>\n</head>\n<body>\n\t{Info}\n</body>\n</html>");
+        }
+    }
+}
--- Exam/Task_2/Program.cs
+++ Exam/Task_2/Program.cs
@@ -10,6 +10,14 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            var page = new HtmlPage() { Name = "Main"};
+            var section = new HtnlSection() { Name = "ur", Info = "<h1>Список:</h1>"};
+            section.AddElement(new HtmlElement() { Name="Страница 1", Info = "<img>Картинка №1</img>"});
+            section.AddElement(new HtmlElement() { Name = "Страница 2", Info = "<img>Картинка №2</img>" });
+            section.AddElement(new HtmlElement() { Name = "Страница 3", Info = "<img>Картинка №3</img>" });
+            page.AddElement(section);
+            Console.WriteLine(page.Print());
+            Console.ReadLine();
         }
     }
 }
